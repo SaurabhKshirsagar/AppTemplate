968f06c4738bcac344c153836c3c1237fb1aa9c8
saurabh_paragyte <skshirsagar@paragyte.com>
2017-05-11 12:20:10 +0530

    get default

--- client/js/helpers/datastore/index.js
+++ client/js/helpers/datastore/index.js
@@ -1,167 +1,187 @@
 import _ from "lodash";
-import {extendObservable, observable,action,isObservable} from 'mobx';
+import { extendObservable, observable, action, isObservable, toJS } from "mobx";
+import { Type, types, onPatch, applyPatch } from "mobx-state-tree";
+
+let getItemModel = function(item, schema) {
+  let defaultitem = {};
+  _.forOwn(schema, function(value, key) {
+    defaultitem[key] = value.defaultVal;
+  });
+  return types.model("item", defaultitem).create(toJS(item));
+};
+let getDefaultNewItem = function(schema) {
+  let item = {};
+  _.forOwn(schema, function(value, key) {
+    item[key] = value.defaultVal;
+  });
+  return item;
+};
 
 class Iterator {
-    constructor(DS,item_Key){
-       extendObservable(this,{
-             first:action(function() {
-                    this.reset();
-                    return this.next();
-              }),
-              getSelected:action(function(){
-                if(this.index== -1)
-                  throw "No item selected";
-                return {key:this.iemKey,item:this.item};
-              }),
-              hasPrev:action(function() {
-                  return this.index > 0;
-              }),
-              reset:action(function() {
-                this.init(this.items);
-              }),
-              each:action(function(callback) {
-                  for (var item = this.first(); this.hasNext(); item = this.next()) {
-                      callback(item);
-                  }
-              })
-       })
-      
-    }
- 
+  constructor(DS, item_Key) {
+    extendObservable(this, {
+      first: action(function() {
+        this.reset();
+        return this.next();
+      }),
+      getSelected: action(function() {
+        if (this.index == -1) throw "No item selected";
+        return { key: this.iemKey, item: this.item };
+      }),
+      hasPrev: action(function() {
+        return this.index > 0;
+      }),
+      reset: action(function() {
+        this.init(this.items);
+      }),
+      each: action(function(callback) {
+        for (var item = this.first(); this.hasNext(); item = this.next()) {
+          callback(item);
+        }
+      })
+    });
+  }
 }
 
 class List extends Iterator {
-    constructor(DS,item_Key){
-      super(DS,item_Key);
-    
-       extendObservable(this,{
-          items:[],
-          index:null,
-          iemKey:null,
-          item:null,
-          isList:action(function(){
-            return true;
-          }),
-          init:action(function(DS,item_Key){
-            this.items=DS;
-            this.index=(item_Key!=""&&item_Key!=null)?item_Key:0;
-            this.iemKey=this.index;
-            
-            this.item=this.items[this.iemKey];
-          }),
-          getByKey:action(function(item_Key){
-            if(item_Key>=0 && item_Key< this.items.length)
-              return this.items[item_Key];
-            throw "Specified key is not present in dataStore";
-          }),
-          next:action(function() {
-            if(!this.hasNext())
-              throw "Data store has no next item"
-              
-            this.index ++;
-            this.iemKey=this.index;
-            this.item=this.items[this.iemKey];
-            return {key:this.iemKey,item:this.item}
-          }),
-          hasNext:action(function() {
-            return this.index < this.items.length;
-          }),
-          prev:action(function() {
-            if(!this.hasPrev())
-              throw "Data store has no prev item"
-              
-            this.index --;
-            this.iemKey=this.index;
-            this.item=this.items[this.iemKey];
-            return {key:this.iemKey,item:this.item}
-          }),
-          //CRUD operations
-          update:action(function(value){
-            this.items[this.iemKey]=value
-          }),
-          delete:action(function(){
-            this.items.splice(this.index,1);
-          })
-       });
-         this.init(DS,item_Key);
+  constructor(DS, schema, item_Key) {
+    super(DS, item_Key);
+
+    extendObservable(this, {
+      items: [],
+      index: null,
+      iemKey: null,
+      item: {},
+      newItem: {},
+      isList: action(function() {
+        return true;
+      }),
+      init: action(function(DS, schema, item_Key) {
+        this.items = DS;
+        this.schema = schema;
+        this.newItem = getDefaultNewItem(schema);
+        this.index = item_Key != "" && item_Key != null ? item_Key : 0;
+        this.iemKey = this.index;
+
+        this.item = this.items[this.iemKey];
+        this.item = getItemModel(this.item, this.schema);
+      }),
+      getByKey: action(function(item_Key) {
+        if (item_Key >= 0 && item_Key < this.items.length)
+          return this.items[item_Key];
+        throw "Specified key is not present in dataStore";
+      }),
+      next: action(function() {
+        if (!this.hasNext()) throw "Data store has no next item";
+
+        this.index++;
+        this.iemKey = this.index;
+        this.item = this.items[this.iemKey];
+        this.item = types.model("item", toJS(this.item)).create();
+        return { key: this.iemKey, item: this.item };
+      }),
+      hasNext: action(function() {
+        return this.index < this.items.length-1;
+      }),
+      prev: action(function() {
+        if (!this.hasPrev()) throw "Data store has no prev item";
+
+        this.index--;
+        this.iemKey = this.index;
+        this.item = this.items[this.iemKey];
+        this.item = types.model("item", toJS(this.item)).create();
+        return { key: this.iemKey, item: this.item };
+      }),
+      //CRUD operations
+      update: action(function(value) {
+        this.items[this.iemKey] = value;
+      }),
+      delete: action(function() {
+        this.items.splice(this.index, 1);
+      })
+    });
+    this.init(DS, schema, item_Key);
+    onPatch(this.item, data => {});
   }
 }
 
 class Map extends Iterator {
-  constructor(DS,item_Key){
-      super(DS,item_Key);
-    
-       extendObservable(this,{
-            items:{},
-            index:null,
-            iemKey:null,
-            item:null,
-            dsMapKeys:[],
-           isMap:action(function(){
-              return true;
-            }),
-            init:action(function(DS,item_Key){
-              this.items=DS;
-              this.dsMapKeys=_.keys(this.items);
-              this.index=(item_Key!=""&&item_Key!=null)
-                            ?_.indexOf(this.dsMapKeys, item_Key)
-                            :0;
-            
-              this.iemKey=this.dsMapKeys[this.index]; 
-              this.item=isObservable(this.items[this.iemKey])?this.items[this.iemKey]:observable(this.items[this.iemKey]);
-            }),
-            getByKey:action(function(item_Key){
-              if(this.items[item_Key]);
-              throw "Specified key is not present in dataStore";
-            }),
-            next:action(function() {
-              if(!this.hasNext())
-                throw "Data store has no next item"
-                
-              this.index ++;
-              this.iemKey=this.dsMapKeys[this.index];
-              this.item=this.items[this.iemKey];
-              return {key:this.iemKey,item:this.item}
-            }),
-            hasNext:action(function() {
-              return this.index < this.dsMapKeys.length;
-            }),
-            prev:action(function() {
-              if(!this.hasPrev())
-                throw "Data store has no prev item"
-                
-              this.index --;
-              this.iemKey=this.dsMapKeys[this.index];
-              this.item=this.items[this.iemKey];
-              return {key:this.iemKey,item:this.item}
-            }),
-            //CRUD operations
-            update:action(function(value){
-              this.items[this.iemKey]=value
-              debugger;
-            }),
-            delete:action(function(){
-              this.dsMapKeys.splice(this.index,1);
-              delete this.items[this.iemKey];
-            })
-       });
-         this.init(DS,item_Key);
+  constructor(DS, schema, item_Key) {
+    super(DS, item_Key);
+
+    extendObservable(this, {
+      items: {},
+      index: null,
+      iemKey: null,
+      item: {},
+      dsMapKeys: [],
+      isMap: action(function() {
+        return true;
+      }),
+      init: action(function(DS, schema, item_Key) {
+        this.items = DS;
+        this.schema = schema;
+        this.newItem = getDefaultNewItem(schema);
+        this.dsMapKeys = _.keys(this.items);
+        this.index = item_Key != "" && item_Key != null
+          ? _.indexOf(this.dsMapKeys, item_Key)
+          : 0;
+
+        this.iemKey = this.dsMapKeys[this.index];
+        this.item = isObservable(this.items[this.iemKey])
+          ? this.items[this.iemKey]
+          : observable(this.items[this.iemKey]);
+        this.item = types.model("item", this.item).create();
+      }),
+      getByKey: action(function(item_Key) {
+        if (this.items[item_Key]);
+        throw "Specified key is not present in dataStore";
+      }),
+      next: action(function() {
+        if (!this.hasNext()) throw "Data store has no next item";
+
+        this.index++;
+        this.iemKey = this.dsMapKeys[this.index];
+        this.item = this.items[this.iemKey];
+        this.item = types.model("item", this.item).create();
+        return { key: this.iemKey, item: this.item };
+      }),
+      hasNext: action(function() {
+        return this.index < this.dsMapKeys.length;
+      }),
+      prev: action(function() {
+        if (!this.hasPrev()) throw "Data store has no prev item";
+
+        this.index--;
+        this.iemKey = this.dsMapKeys[this.index];
+        this.item = this.items[this.iemKey];
+        this.item = types.model("item", this.item).create();
+        return { key: this.iemKey, item: this.item };
+      }),
+      //CRUD operations
+      update: action(function(value) {
+        this.items[this.iemKey] = value;
+      }),
+      delete: action(function() {
+        this.dsMapKeys.splice(this.index, 1);
+        delete this.items[this.iemKey];
+      })
+    });
+    this.init(DS, schema, item_Key);
+    onPatch(this.item, data => {
+      debugger;
+    });
   }
 }
 
 //Singleton Object of DataStore
-let DS=(function(){
-   return {
-        of: function (ds,item_Key) {
-           if(_.isArray(ds))
-              return new List(ds,item_Key);
-           return new Map(ds,item_Key);
-        }
-    };
+let DS = (function() {
+  return {
+    of: function(ds, item_Key) {
+      if (_.isArray(ds)) return new List(ds, item_Key);
+      return new Map(ds, item_Key);
+    }
+  };
 })();
 
-
-module.exports={DS}
-
-
-
+module.exports = { DS };
